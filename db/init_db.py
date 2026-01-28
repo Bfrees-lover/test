0f9813e55dcc1f68b04b381866a5174d155c8c3f
Bfrees-Lover <dimonciksamarinskij751@gmail.com>
2026-01-28 15:39:16 +0300

    Docker + Nginx + PHP stack

--- db/init_db.py
+++
@@ -1,9 +0,0 @@
-from core.models import Base
-from db.database import engine
-
-def init_db():
-    Base.metadata.create_all(bind=engine)
-    print("✅ База данных match3.db создана.")
-
-if __name__ == "__main__":
-    init_db()
--- main.py
+++
@@ -1,7 +0,0 @@
-from db.init_db import init_db
-from gui.main_window import MainWindow
-
-if __name__ == "__main__":
-    init_db()
-    app = MainWindow()
-    app.run()
